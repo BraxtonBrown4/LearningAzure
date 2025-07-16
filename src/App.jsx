07e9b1cf6347b00d7d4a2a05b0a3dbe515e5e78e
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-07-16 13:59:10 -0500

    lag waggan

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 
 function App() {
   const [fetchStatus, setFetchStatus] = useState("")
+  const [data, setData] = useState({})
 
   const func = async () => {
   try {
@@ -14,7 +15,8 @@ function App() {
     }
 
     const data = await res.json();
-    setFetchStatus(data.activation)
+    setData(data)
+    setFetchStatus("Success")
   } catch (error) {
     setFetchStatus(`Network error: ${error.message}`);
   }
@@ -22,14 +24,12 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Lag Waggon Press The Button</h1>
       <div className="card">
         <button onClick={func}>
           Status: {fetchStatus}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        {data.activation && data.invasions.map(i => <p>{i.ally}</p>)}
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
